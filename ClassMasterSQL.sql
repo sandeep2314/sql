83d1745380a50faf9d3a8659708007014d9066b7
sandeep2314 <47693625+sandeep2314@users.noreply.github.com>
2019-04-07 17:14:03 +0530

    full class master data

--- ClassMasterSQL.sql
+++ ClassMasterSQL.sql
@@ -96,6 +96,92 @@ SET @cOrder = @cOrder+ 10
 INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
 VALUES('V C', @cOrder+ 10,  @UID, 2014)
 
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VI A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VI B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VI C', @cOrder+ 10,  @UID, 2014)
+
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VII A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VII B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VII C', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VIII A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VIII B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('VIII C', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('IX A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('IX B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('IX C', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('X A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('X B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('X C', @cOrder+ 10,  @UID, 2014)
+
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XI A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XI B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XI C', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XII A', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XII B', @cOrder+ 10,  @UID, 2014)
+
+SET @cOrder = @cOrder+ 10
+INSERT INTO tblClassMaster (ClassName, ClassOrder, USERID, Fyear) 
+VALUES('XII C', @cOrder+ 10,  @UID, 2014)
+
 
 
 
