646d5964e01cd08cc77c08cb70f20a3f3ac9a367
sandeep2314 <47693625+sandeep2314@users.noreply.github.com>
2019-04-11 10:00:16 +0530

    add student master

---
+++ Stock/AccountingSoftware/Master/StudentMaster.aspx
@@ -0,0 +1,255 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true"
+ CodeBehind="StudentMaster.aspx.cs" 
+ MasterPageFile="~/SiteMaster.Master" 
+ Inherits="AccountingSoftware.Master.StudentMaster" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <link href="../css/MyCss.css" rel="stylesheet" type="text/css" />
+    <link href="../js/jquery-ui-1.8.17.custom.css" rel="stylesheet" type="text/css" />
+    <script src="../js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="../js/jquery-ui-1.8.17.custom.min.js" type="text/javascript"></script>
+    <script src="../js/jquery.ui.autocomplete.js" type="text/javascript"></script>
+    <script src="../js/jquery.ui.widget.js" type="text/javascript"></script>
+    <script src="../js/jquery.ui.core.js" type="text/javascript"></script>
+    <script src="../js/jquery.ui.position.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#ctl00_ContentPlaceHolder1_txtSearch").keypress(function (e) {
+                if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+                    $("#ctl00_ContentPlaceHolder1_btnSearch").click();
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+
+          )
+        });
+    </script>
+    <script type="text/javascript">
+
+        $(function () {
+            $("#<%= txtDOB.ClientID  %>").datepicker({
+                showOn: "button",
+                buttonImage: "../images/calendar.gif",
+                buttonImageOnly: true
+            });
+        });
+
+    </script>
+    
+     <div class="auth-div">
+             <table>
+                  <tr>
+                  <td>
+                 <a runat="server" id="hrfAuth">AuthorisedDealer.com</a>
+                  </td></tr>
+                    <tr><td>
+                        <asp:Label ID="lblSupport" runat="server" 
+                        
+                        Text="For Support: Mob. 921 948 4030 Email: AuthDealer@gmail.com" 
+                        ForeColor="#666666" Font-Size="9pt"></asp:Label> 
+                    </td></tr>
+                 
+            </table>
+    </div>
+
+    <table width="100%">
+        <tr>
+            <td>
+                <div class="button-div">
+                    
+                    <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" CssClass="button" />
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click"
+                        CssClass="button" />
+                    <asp:Button ID="btnGet" runat="server" Text="Get" OnClick="btnGet_Click"
+                        CssClass="button" />
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnCancel_Click" CssClass="button" />
+                    <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+                    <asp:Button ID="btnSearch" runat="server" Text="Search" CssClass="button" OnClick="btnSearch_Click" />
+                    <asp:Button ID="btnChangeClass" runat="server" Text="Change Class" CssClass="button" OnClick="btnChangeClass_Click" />
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div id="errDiv" class="errDiv">
+                    <asp:Label ID="lblError" runat="server" CssClass="error-lable" Visible="False"></asp:Label>
+                    <asp:Label ID="lblMessage" runat="server" CssClass="message-lable" Visible="False"></asp:Label>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="form-div">
+                    <fieldset>
+                     <legend class="legend">Student Master</legend>                        
+                        <table width="100%">
+                            <tr>
+                                <td class="tblcontent">
+                                    Student Master Id
+                                </td>
+                                <td>
+                                    <asp:Label ID="lblStudentMasterId" runat="server" Text="New"></asp:Label>
+                                </td>
+                                 <td class="tblcontent">
+                                 
+                                </td>
+                                <td>
+                                 
+                                    
+                                </td>
+                                 
+                            </tr>
+                             <tr>
+                               
+                                <td class="tblcontent">
+                                    Name 
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="txtStudentName" runat="server"></asp:TextBox>
+                                    
+                                </td>
+                                 
+                                  <td class="tblcontent">
+                                    Adm. No
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="txtAdmNo" runat="server"></asp:TextBox>
+                                    
+                                </td>
+
+
+                            </tr>
+                            <tr>
+                             <td class="tblcontent">
+                                    Present Class
+                                </td>
+                                <td>
+                                    
+                                    <asp:DropDownList ID="ddlPresentClass" runat="server"></asp:DropDownList>
+                                </td>
+
+                                 <td class="tblcontent" valign="top">
+                                    Section
+                                </td>
+                                <td>
+                                    <asp:DropDownList ID="ddlSection" runat="server"></asp:DropDownList>
+                                </td>
+                               
+                               
+                            </tr>
+                            <tr>
+                               
+                                <td class="tblcontent">
+                                    Mobile No(Primary)
+                                </td>
+                                <td>
+                                   <asp:TextBox ID="txtMobileNoF" MaxLength="10"  runat="server"></asp:TextBox>
+                                   
+                                </td>
+
+                                <td class="tblcontent">
+                                    Mobile No(Secondary)
+                                </td>
+                                <td>
+                                   <asp:TextBox ID="txtMobileNoM" runat="server"></asp:TextBox>
+                                   
+                                </td>
+                               
+
+                            </tr>
+                            
+
+                            <tr>
+
+                             <td class="tblcontent">
+                                    Mother's Name
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="txtMotherName" runat="server"></asp:TextBox>
+                                </td>
+
+                                  <td class="tblcontent" valign="top">
+                                    Father's Name
+                                </td>
+                                <td>
+                                     <asp:TextBox ID="txtFatherName" runat="server"></asp:TextBox>
+                                </td>
+
+                                </tr>
+
+
+                            <tr>
+
+                             <td class="tblcontent">
+                                    DOB
+                                </td>
+                                <td>
+                                    <asp:TextBox ID="txtDOB" runat="server"></asp:TextBox>
+                                </td>
+
+                                  <td class="tblcontent" valign="top">
+                                    ID Card No
+                                </td>
+                                <td>
+                                     <asp:TextBox ID="txtIdCardNo" runat="server"></asp:TextBox>
+                                </td>
+
+                                </tr>
+                            
+                            
+                        </table>
+                    </fieldset>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="grid-div" style="top: 55%">
+                    <asp:GridView ID="gridView1" runat="server" AutoGenerateColumns="false" Width="100%"
+                       AllowPaging="true" CssClass="mGrid" PagerStyle-CssClass="pgr"
+                        GridLines="None" AlternatingRowStyle-CssClass="alt" EnableModelValidation="True"
+                        OnSelectedIndexChanged="gridView1_SelectedIndexChanged" OnPageIndexChanging="grid1_PageIndexChanging"
+                        PageSize="2000">
+                        <Columns>
+                            <asp:TemplateField HeaderText="Delete" HeaderStyle-Width="20">
+                                <ItemTemplate>
+                                    <asp:CheckBox ID="myCheckBox" runat="server" />
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:CommandField SelectText="Edit" ItemStyle-CssClass="links" ShowSelectButton="True"
+                                HeaderText="Edit" HeaderStyle-Width="20" />
+
+                          <asp:TemplateField HeaderText="SrNo" HeaderStyle-Width="10">
+                           <HeaderTemplate>
+                               <asp:Label ID="srno1"  runat="server" Text="Sr.No."  />
+                           </HeaderTemplate>
+                           <ItemTemplate>
+                               <asp:Label ID="lblSRNO" runat="server" Text='<%#Container.DataItemIndex+1 %>'></asp:Label>
+                            </ItemTemplate>
+                         </asp:TemplateField>
+
+                            <asp:BoundField HeaderText="ID" DataField="StudentMasterId" />
+                            <asp:BoundField HeaderText="Admn No" DataField="AdmNo" HeaderStyle-Width="20" />
+                            <asp:BoundField HeaderText="Student Name" DataField="StudentName" />
+                            <asp:BoundField HeaderText="Present Class" DataField="PresentClassName" />
+                            <asp:BoundField HeaderText="Section" DataField="Section" />
+                            <asp:BoundField HeaderText="MotherName" DataField="MotherName" />
+                            <asp:BoundField HeaderText="FatherName" DataField="FatherName" />
+                            <asp:BoundField HeaderText="MobileNo(Father)" DataField="MobileNoF" />
+                         
+                            <asp:BoundField HeaderText="DOB" DataField="DOB" />
+                            
+                        </Columns>
+                    </asp:GridView>
+                </div>
+            </td>
+        </tr>
+    </table>
+   
+</asp:Content>
---
+++ Stock/AccountingSoftware/Master/StudentMaster.aspx.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using AccountingSoftware.BLL;
+
+
+namespace AccountingSoftware.Master
+{
+    public partial class StudentMaster : System.Web.UI.Page
+    {
+        static DBSite site = null;
+        Util_BLL util = new Util_BLL();
+        static bool edit_mode = false;
+        static bool isErr = false;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                site = new DBSite();  
+               util.FillClass(site, ddlPresentClass);
+
+               SectionMasterBll secBll = new SectionMasterBll();
+               List<SectionMasterBll.SectionMasterEntity> secList = secBll.GetSectionList(site, Util_BLL.User.UserId);
+               
+
+               foreach (SectionMasterBll.SectionMasterEntity sec in secList)
+               {
+                   ddlSection.Items.Add(new ListItem(sec.SectionName, sec.SectionMasterId.ToString()));
+               }
+
+
+
+            }
+
+        }
+
+
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!IsValidForm())
+            {
+                ClearSuccessMessage();
+                return;
+            }
+            SaveStudent();
+            if (edit_mode)
+                GetStudents();
+            if(!isErr)           
+                Clear();
+        }
+
+
+        public bool IsDuplicateCardNo(DBSite site, string cardNo, int studentId)
+        {
+            StudentBll sBll = new StudentBll();
+            if (sBll.IsDuplicateCard(site, cardNo, studentId ))
+            {
+                lblError.Visible = true;
+                lblError.Text = "Id Card No '" + cardNo + "' already exists in our System.";
+                txtIdCardNo.Focus();
+
+                return true;
+            }
+            return false;
+        }
+
+
+        protected bool IsValidForm()
+        {
+
+            string errMsg = " Please Enter ";
+            int length = errMsg.Length;
+
+            bool isErr = false;
+
+
+
+            if (!util.IsValidName(txtStudentName.Text))
+            {
+                if (errMsg.Length > length)
+                    errMsg += ", " + Environment.NewLine;
+                errMsg += "Valid Student Name";
+                isErr = true;
+            }
+
+
+
+
+           
+           
+
+            lblError.Text = errMsg;
+            lblError.Visible = isErr;
+
+            return !isErr;
+
+        }
+
+
+
+
+        protected void SaveStudent()
+        {
+            StudentBll st_bll = null;
+
+            st_bll = new StudentBll();
+            StudentBll.StudenEntity student = new StudentBll.StudenEntity();
+
+            try
+            {
+
+                site = new DBSite();
+                site.BeginTrans();
+        
+                student.StudentName  = txtStudentName.Text ;
+                student.AdmNo = txtAdmNo.Text;
+                student.PresentClass = util.CheckNullInt(ddlPresentClass.SelectedValue);
+
+                student.SectionId = util.CheckNullInt(ddlSection.SelectedValue);
+
+                student.IdCardNo = txtIdCardNo.Text;
+                student.FatherName = txtFatherName.Text;
+                student.MotherName = txtMotherName.Text;
+
+                student.MobileNoF = txtMobileNoF.Text;
+                student.MobileNoM = txtMobileNoM.Text;
+                student.DOB = txtDOB.Text;
+
+                if (txtMobileNoF.Text.Length != 10)
+                {
+                    lblError.Text = "Mobile Number should be of 10 digits";
+                    lblError.Visible = true;
+                    txtMobileNoF.Focus();
+                    ClearSuccessMessage();
+                    return;
+                }
+                
+
+
+                student.Email = txtDOB.Text;
+
+                
+                string message = "";
+
+
+                
+                
+                if (this.lblStudentMasterId.Text == "New")
+                {
+                   
+
+                    //if (IsDuplicateCardNo(site, student.IdCardNo, -1))
+                    //{
+                    //    txtIdCardNo.Focus();
+                    //    isErr = true;
+                       
+                    //    return;
+                    //}
+
+                    st_bll.SaveStudent(site, student);
+                    message = " Successfully Added !";
+                    
+                }
+                else
+                {
+                    student.StudentMasterId = util.CheckNullInt(lblStudentMasterId.Text);
+
+                    //if (IsDuplicateCardNo(site, student.IdCardNo, student.StudentMasterId))
+                    //{
+                    //    txtIdCardNo.Focus();
+                    //    isErr = true;
+                    //    return;
+                    //}
+                    
+                    st_bll.UpdateStudent(site, student);
+                    message = " Successfully Updated !";
+                }
+
+                site.Commit();
+                lblMessage.Text = message;
+                lblMessage.Visible = true;
+                isErr = false;
+                ClearErrorMessage();
+            }
+            catch (Exception ex)
+            {
+                site.RollBack();
+                util.HandleExpception(ex, Response);
+                
+            }
+            finally
+            {
+                site.Close();
+            }
+        
+        
+        }
+
+
+        protected void btnGet_Click(object sender, EventArgs e)
+        {
+            
+            GetStudents();
+            ClearAll();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            site = new DBSite();
+            StudentBll stBll = new StudentBll();
+
+            List<string> student_id_list = new List<string>();
+            try
+            {
+                foreach (GridViewRow gr in gridView1.Rows)
+                {
+                    CheckBox cb = (CheckBox)gr.Cells[0].FindControl("myCheckBox");
+                    if (cb.Checked)
+                    {
+                        //if (stBll.IsStudentPresentInAttendance(site, util.CheckNullInt(gr.Cells[2].Text)))
+                        //{
+                        //    lblError.Text = "Can't Delete Record:'" + gr.Cells[4].Text + "' This Student is being entered in Attendance Entry.";
+                        //    lblError.Visible = true;
+                        //    ClearSuccessMessage();
+                        //    return;
+                        //}
+
+                        student_id_list.Add(gr.Cells[3].Text);
+                    }
+                }
+
+
+                if (student_id_list.Count < 1)
+                {
+                    lblError.Text = "Can't Delete Records. Select at least one Record  ";
+                    lblError.Visible = true;
+                    ClearSuccessMessage();
+                    return;
+                }
+
+                string st_ids = util.ConvertListToCommaSeparatedString(student_id_list);
+
+
+                site.BeginTrans();
+                stBll.DeleteStudent(site, st_ids);
+                site.Commit();
+
+                lblMessage.Text = " [ " + student_id_list.Count + "] Record Deleted ";
+                lblMessage.Visible = true;
+                ClearErrorMessage();
+            }
+            catch (Exception del_exception)
+            {
+                site.RollBack();
+                util.HandleExpception(del_exception, Response);
+            }
+            finally
+            {
+                site.Close();
+            }
+            GetStudents();
+        }
+
+
+        protected void btnChangeClass_Click(object sender, EventArgs e)
+        {
+            site = new DBSite();
+            StudentBll stBll = new StudentBll();
+            List<string> student_id_list = new List<string>();
+
+            
+
+
+            foreach (GridViewRow gr in gridView1.Rows)
+            {
+                CheckBox cb = (CheckBox)gr.Cells[0].FindControl("myCheckBox");
+                if (cb.Checked)
+                {
+                    //if (stBll.IsStudentPresentInAttendance(site, util.CheckNullInt(gr.Cells[2].Text)))
+                    //{
+                    //    lblError.Text = "Can't Delete Record:'" + gr.Cells[4].Text + "' This Student is being entered in Attendance Entry.";
+                    //    lblError.Visible = true;
+                    //    ClearSuccessMessage();
+                    //    return;
+                    //}
+
+                    student_id_list.Add(gr.Cells[3].Text);
+                }
+            }
+
+
+            if (student_id_list.Count < 1)
+            {
+                lblError.Text = "Can't Delete Records. Select at least one Record  ";
+                lblError.Visible = true;
+                ClearSuccessMessage();
+                return;
+            }
+
+            string st_ids = util.ConvertListToCommaSeparatedString(student_id_list);
+
+            if (util.CheckNullInt(ddlPresentClass.SelectedValue )!=0)
+                stBll.changeClass(site, util.CheckNullInt(ddlPresentClass.SelectedValue), st_ids);
+            
+        }
+        
+
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            site = new DBSite();
+            StudentBll stBll = new StudentBll();
+           
+            // 777
+            List<StudentBll.StudenEntity> students = stBll.SearchStudents(site, txtSearch.Text );
+           // total_records = students.Count;
+            gridView1.DataSource = students;
+            gridView1.DataBind();
+
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+           
+        }
+
+        protected void ClearAll()
+        {
+            Clear();
+            lblMessage.Text = string.Empty;
+            lblMessage.Visible = false;
+
+            lblError.Visible = false;
+            lblError.Text = string.Empty;
+        }
+
+        protected void btnSendSMS_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void gridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            ClearAll();
+            edit_mode = true;
+            GridViewRow row = gridView1.SelectedRow;
+            lblStudentMasterId.Text = row.Cells[3].Text;
+            
+            StudentBll sBll = new StudentBll();
+
+            site = new DBSite();
+
+            try
+            {
+                //StudentBll.StudenEntity st = sBll.GetStudents(site, util.CheckNullInt(lblStudentMasterId.Text))[0];
+                StudentBll.StudenEntity st = sBll.GetStudents(site, lblStudentMasterId.Text)[0];
+
+                txtStudentName.Text = st.StudentName;
+                txtAdmNo.Text = st.AdmNo;
+                ddlPresentClass.SelectedValue = util.CheckNull(st.PresentClass);
+                txtIdCardNo.Text = st.IdCardNo;
+                txtMotherName.Text = st.MotherName;
+                txtFatherName.Text = st.FatherName;
+                txtMobileNoF.Text = st.MobileNoF;
+                txtMobileNoM.Text = st.MobileNoM;
+                txtDOB.Text = st.DOB;
+                
+             }
+            catch (Exception exception)
+            {
+                util.HandleExpception(exception, Response);
+               // throw;
+            }
+            finally
+            {
+                site.Close();
+            }
+
+
+        }
+
+        protected void grid1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridView1.PageIndex = e.NewPageIndex;
+            gridView1.DataBind();
+
+            GetStudents();
+      
+        }
+
+
+        protected void ClearSuccessMessage()
+        {
+            lblMessage.Text = string.Empty;
+            lblMessage.Visible = false;
+        }
+
+        protected void ClearErrorMessage()
+        {
+            lblError.Visible = false;
+            lblError.Text = string.Empty;
+        }
+
+
+        protected void Clear()
+        {
+
+
+            lblStudentMasterId.Text = "New";
+            this.txtStudentName.Text  = String.Empty;
+            this.txtIdCardNo.Text  = String.Empty;
+            this.txtMotherName.Text = String.Empty;
+            this.txtFatherName.Text = String.Empty;
+            this.txtMobileNoF.Text = String.Empty;
+            this.txtMobileNoM.Text = String.Empty;
+            this.txtDOB.Text = String.Empty;
+            
+
+        }
+
+
+        private void GetStudents()
+        {
+            site = new DBSite();
+            StudentBll stBll = new StudentBll();
+
+            string classIDs = ddlPresentClass.SelectedValue;
+
+            
+
+            //try
+            {
+               // List<StudentBll.StudenEntity>  students = stBll.GetStudents(site);
+                List<StudentBll.StudenEntity> students = stBll.GetStudentsByClass(site, "", classIDs);
+                //total_records = students.Count;
+                gridView1.DataSource = students;
+                gridView1.DataBind();
+            }
+            //catch (Exception exception)
+            //{
+            //    util.HandleExpception(exception, Response);
+            //}
+            //finally
+            //{
+            //    site.Close();
+            //}
+
+        }
+
+    }
+}
